b7597dd7b0574bbc251a833d39ff0936bc554111
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-08-14 19:07:24 +0300

    homework for_lesson_4

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'project',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -154,4 +155,8 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
 
     ),
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
--- TODO/project/views.py
+++ TODO/project/views.py
@@ -1,18 +1,50 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from project.filters import ProjectFilter, TodoFilter
 from project.models import Project, Todo
 from project.serializer import ProjectSerializer, TodoSerializer
 
 
-class ProjectModelViewSet(ModelViewSet):
-    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+class ProjectModelViewSet(viewsets.ModelViewSet):
+    # renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name', None)
+        queryset = Project.objects.all()
+        if name:
+            queryset = queryset.filter(name__contains=name)
+        return queryset
 
 
-class TodoModelViewSet(ModelViewSet):
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+class TodoCustomViewSet(mixins.CreateModelMixin,mixins.RetrieveModelMixin,
+                        mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    pagination_class = TodoLimitOffsetPagination
+    filterset_class = TodoFilter
+
+    def desrtoy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.is_active = False
+            instance.save()
+        except:
+            Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            Response(status=status.HTTP_204_NO_CONTENT)
