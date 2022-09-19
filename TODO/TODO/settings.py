4625d90cd94f8b09afbfd01fb13350dc85465c3f
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-09-19 10:18:07 +0300

    Homework for lesson 12

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://127.0.0.1:3000',
-    'http://192.168.0.16:3000',
+    'http://0.0.0.0:80',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -89,12 +91,24 @@ WSGI_APPLICATION = 'TODO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'PASSWORD': '1',
+        'USER': 'django',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- TODO/users/management/commands/create_superuser_and-users.py
+++ TODO/users/management/commands/create_superuser_and-users.py
@@ -14,11 +14,11 @@ class Command(BaseCommand):
         return ''.join(random.choice(string.ascii_lowercase) for i in range(8)) + '@gmail.com'
 
     def handle(self, *args, **options):
-        User.objects.create_superuser(username='superuser: ' + get_random_string(5),
-                                      email=self.get_email(),
-                                      first_name=get_random_string(5),
-                                      last_name=get_random_string(5),
-                                      password='1')
+        # User.objects.create_superuser(username='superuser: ' + get_random_string(5),
+        #                               email=self.get_email(),
+        #                               first_name=get_random_string(5),
+        #                               last_name=get_random_string(5),
+        #                               password='1')
         for i in range(number):
             User.objects.create_user(username=get_random_string(5),
                                      email=self.get_email(),
